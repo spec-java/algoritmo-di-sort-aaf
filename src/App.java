7d4b46dbfe70be16bb7dbb30722f45b9ed3a52ba
AlessandroSottosanti <asottosanti2@gmail.com>
2025-05-07 14:19:29 +0200

    refactoring: sortingNumbersMethod()

--- src/App.java
+++ src/App.java
@@ -1,46 +1,71 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
-        // System.out.println("Hello, World!");
-
         int[] arrayInt = { 4, 9, 3, 7 };
+        String[] arrayString  = {"piero", "pierone", "pierluigi", "piergianpaolo"};
 
-        sortingMethod(arrayInt);
-        // for (int num : arrayInt) {
-        // System.out.println(num);
-        // }
+        System.out.println("Ordinamento array numerico: ");
+        sortingNumbersMethod(arrayInt);
+
+        System.out.println("Ordinamento array di stringhe per lunghezza:");
+        sortingStringsMethod(arrayString);
+
     }
 
-    public static int[] sortingMethod(int[] arrayInt) {
-        int[] lastArray = new int[arrayInt.length];
+    public static int[] sortingNumbersMethod(int[] arrayInt) {
         int[] sortedArray = new int[arrayInt.length];
-        int higherNum = 0;
 
         for (int i = 0; i < arrayInt.length; i++) {
+            int lowerNum = 100;
+            int lastNumIndex = 0;
 
-            sortedArray[i] = arrayInt[i];
+            for (int y = 0; y < arrayInt.length; y++) {
+                int currentNum = arrayInt[y];
 
-        }
+                if (lowerNum > currentNum) {
+                    lowerNum = currentNum;
+                    lastNumIndex = y;
 
-        for (int i = 0; i < sortedArray.length; i++) {
-            int currentNum = sortedArray[i];
-
-            if(higherNum < currentNum){
-                higherNum  = currentNum;
-                lastArray[i] = higherNum;
-            //  currentNum = 0;
+                }
 
             }
-
-            // higherNum = 0;
+            arrayInt[lastNumIndex] = 100;
+            sortedArray[i] = lowerNum;
 
         }
-        System.out.println(higherNum);
 
-         for (int num : lastArray) {
-             System.out.println(num);
-         }
+        for (int num : sortedArray) {
+            System.out.println(num);
+        }
 
-        return arrayInt;
+        return sortedArray;
+    }
+
+    public static String[] sortingStringsMethod(String[] arrayString) {
+        String[] sortedArray = new String[arrayString.length];
+
+        for (int i = 0; i < arrayString.length; i++) {
+            String lowestString = "aklnbsdlknsdannaslsaknlasklsa";
+            int lastStringIndex = 0;
+
+            for (int y = 0; y < arrayString.length; y++) {
+                String currentString = arrayString[y];
+
+                if (lowestString.length() > currentString.length()) {
+                    lowestString = currentString;
+                    lastStringIndex = y;
+
+                }
+
+            }
+            arrayString[lastStringIndex] = "aklnbsdlknsdannaslsaknlasklsa";
+            sortedArray[i] = lowestString;
+
+        }
+
+        for (String string : sortedArray ) {
+            System.out.println(string + ", lunghezza: " + string.length());
+        }
+
+        return sortedArray;
     }
 }
